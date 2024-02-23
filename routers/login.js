5c4d24a0631329e32960146e4f0de350d1dbb47d
Daniele-Cereghetti <61267822+Daniele-Cereghetti@users.noreply.github.com>
2024-02-23 16:29:18 +0100

    Validation login inputs

--- routers/login.js
+++ routers/login.js
@@ -1,16 +1,18 @@
 const express = require('express')
 const router = express.Router()
 const crypt = require('crypto')
+const { body, matchedData, validationResult } = require('express-validator');
 const sqlite = require('sqlite3').verbose()
 
 let db = new sqlite.Database(__dirname + '/../db/classimg.db')
 
-router.post('/logon', (req,res)=>{
+router.post('/logon', body('username').notEmpty().trim().escape(),body('passwd').notEmpty().trim().escape(), (req,res)=>{
     if(!req.session.username){
-        let user = req.body.username
-        let passwd = req.body.passwd
-        if(user && passwd){
-            passwd = crypt.createHash('sha256').update(passwd).digest('hex').toString()
+        const result = validationResult(req);
+        if(result.isEmpty()){
+            const data = matchedData(req);
+            let user = data.username
+            let passwd = crypt.createHash('sha256').update(data.passwd).digest('hex').toString()
             getUser(user, passwd, (usr) => {
                 if(usr){
                     if(usr.blocked == 0){
@@ -26,7 +28,7 @@ router.post('/logon', (req,res)=>{
                 }
             })
         }else{
-            res.render('index', {loginError: "Errore: Non tutti i campi sono stati riempiti"})
+            res.status(400).render('index', {loginError: "Errore: Non tutti i campi sono stati riempiti"})
         }
     }else{
         res.redirect("/img")
