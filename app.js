8b1ed16e89bfa3f5dee08a90bdb358eee1e45565
Daniele-Cereghetti <61267822+Daniele-Cereghetti@users.noreply.github.com>
2024-02-23 12:16:49 +0100

    fixed problem with bootstrap css

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const session = require('express-session')
 const upload = require('express-fileupload')
+const path = require('path');
 
 const app = express()
 const port = 3000
@@ -8,10 +9,17 @@ const port = 3000
 // requisito prossimo --> menu utente (reset password, ecc.)
 
 app.set('view engine', 'ejs')
-
-app.use(express.static("images"))
-app.use(express.static("style"))
-app.use(express.urlencoded({ extended : true }))
+app.use(express.static(path.join(__dirname, "images")))
+app.use(express.static(path.join(__dirname, "style")))
+app.use(express.static(path.join(__dirname, 'node_modules/bootstrap-icons/font/'), {
+    setHeaders: function (res, path, stat) {
+      if (path.endsWith('.css')) {
+        // Set MIME type for CSS files to 'text/css'
+        res.set('Content-Type', 'text/css');
+      }
+    }
+}))
+app.use(express.urlencoded({ extended : true, limit: '10mb' }))
 app.use(session({
     secret : 'exsperandos',
     resave : false, 
@@ -22,10 +30,12 @@ app.use(upload())
 const imgRouter = require('./routers/img')
 const loginRouter = require('./routers/login')
 const adminRouter = require('./routers/admin')
+const userRouter = require('./routers/user')
 
 app.use("/img", imgRouter)
 app.use("/login", loginRouter)
 app.use("/admin", adminRouter)
+app.use("/user", userRouter)
 
 app.get('/', (req,res)=>{
     if(req.session.username){
